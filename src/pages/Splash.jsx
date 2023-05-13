e3f78f6cd1e10a7123bca700804a45679ffbf05c
Piyush Yadav <piyushya012@gmail.com>
2023-05-13 19:57:45 +0530

    styled login page with theme

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { useNavigate } from "react-router-dom";
 import '../styles/splash.css'
 import ncsLogo from '../assets/ncs-logo.png'
+import alertify from 'alertifyjs'; 
+import 'alertifyjs/build/css/alertify.css';
 
 export default function Splash(){
     const navigate = useNavigate();
+    React.useEffect(() => {
+        alertify.set('notifier','position', 'bottom-center');
+        alertify.notify('Click anywhere to cntinue', 'success', 5, function(){  console.log('dismissed'); });
+    }, [])
     function handleClick(){
+        alertify.dismissAll();
         navigate("/login"); 
     }
     return(
@@ -17,12 +24,13 @@ export default function Splash(){
 */
 
         <div className = "splashContainer" onClick={handleClick}>
-            <img className="ncsLogo" src={ncsLogo} alt="Nibble Computer Society Logo"/>
-            <div className='greetings-container'>
-                <h1 className='welcomeText'>Welcome</h1>
-                <h2 className='farewellText'>Farewell 2k23</h2>
+            <div className='upperHalf'>
+                <img className="ncsLogo" src={ncsLogo} alt="Nibble Computer Society Logo"/>
+                <div className='greetings-container'>
+                    <h1 className='welcomeText'>Welcome</h1>
+                    <h2 className='farewellText'>Farewell 2k23</h2>
+                </div>
             </div>
-            <p className='clickText'>Click to go to login Window</p>
         </div>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,8 +7,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import '../styles/login.css'
 import alertify from 'alertifyjs';
 import 'alertifyjs/build/css/alertify.css';
-
-alertify.set('notifier','position', 'top-center');
+import ncsLogo from '../assets/ncs-logo.png'
 
 export default function Login(){
     const [formData, setFormData] = React.useState({
@@ -17,6 +16,7 @@ export default function Login(){
     })
     const [user, loading] = useAuthState(auth);
     const navigate = useNavigate();
+
     React.useEffect(() => {
         if (loading) {
             console.log("loading")
@@ -24,6 +24,7 @@ export default function Login(){
         }
         if (user){
             navigate("/dashboard");
+            alertify.set('notifier','position', 'top-center');
             alertify.notify('Login Successful', 'success', 2, function(){  console.log('dismissed'); });
         }
     }, [user, loading]);
@@ -39,7 +40,16 @@ export default function Login(){
     }
 
     async function handleSubmit(e){
+        alertify.set('notifier','position', 'top-center');
         e.preventDefault()
+        if(formData.email === ""){
+            alertify.notify('Empty Email field', 'warning', 2, function(){  console.log('dismissed'); });
+            return
+        }
+        if(formData.password === ""){
+            alertify.notify('Empty password field', 'warning', 2, function(){  console.log('dismissed'); });
+            return
+        }
         logInWithEmailAndPassword(formData.email, formData.password)
         // logInWithEmailAndPassword(email, password)
     }
@@ -54,24 +64,28 @@ export default function Login(){
 
         <div className='formContainer'>
             <form className="loginForm" onSubmit={handleSubmit}>
-                <input 
-                    id = "email"
-                    className='inputField'
-                    type = "email"
-                    name = "email"
-                    value = {formData.email}
-                    placeholder='Email'
-                    onChange = {handleChange}
-                />
-                <input 
-                    id = "password"
-                    className='inputField'
-                    type = "password"
-                    name = "password"
-                    value = {formData.password}
-                    placeholder='Password'
-                    onChange = {handleChange}
-                />
+                <img className="ncsLogo" src={ncsLogo} alt="Nibble Computer Society Logo"/>
+                <h1 className='loginWelcome'>Welcome</h1>
+                <div className='inputContainer'>
+                    <input 
+                        id = "email"
+                        className='inputField'
+                        type = "email"
+                        name = "email"
+                        value = {formData.email}
+                        placeholder='Email'
+                        onChange = {handleChange}
+                    />
+                    <input 
+                        id = "password"
+                        className='inputField'
+                        type = "password"
+                        name = "password"
+                        value = {formData.password}
+                        placeholder='Password'
+                        onChange = {handleChange}
+                    />
+                </div>
                 <button className='loginButton'>Login</button>
             </form>
         </div>
